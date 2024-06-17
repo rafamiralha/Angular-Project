d34612bac7baa5556c669d773727d3eb080b27d5
Rafael Miralha <rafamiralha1@gmail.com>
2024-06-17 16:59:02 -0300

    feat: add config to novoProd

--- TerceiroApp/src/app/nov-prod/nov-prod.component.ts
+++ TerceiroApp/src/app/nov-prod/nov-prod.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nov-prod',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './nov-prod.component.html',
   styleUrl: './nov-prod.component.css'
 })
-export class NovProdComponent {
+export class NovProdComponent implements OnInit {
+  router: Router;
 
+  constructor(router: Router){this.router = router; }
+  incluir():void{
+    this.router.navigateByUrl("/produto")
+    }
+  ngOnInit(): void {
+
+}
 }
